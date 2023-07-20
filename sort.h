c58b59207e9cc7b31a3485b1243f212502c0dd4b
Codemartino <akudugumartin5@gmail.com.com>
2023-07-20 10:07:25 -0700

    Header file

--- sort.h
+++ sort.h
@@ -1,27 +1,36 @@
-#ifndef _SORT_H_
-#define _SORT_H_
+#ifndef _SORTING_H_
+#define _SORTING_H_
+#include <stdio.h>
 
-#include <stdlib.h>
+
+/** Struct Double Linked List */
 
 /**
- * struct listint_s - doubly-linked list node
+ * struct listint_s - doubly linked list node
  *
- * Akudugu Martin Project
+ * Martin Akudugu
  * @n: integer stored in the node
  * @prev: pointer to the previous element of the list
  * @next: pointer to the next element of the list
  */
 typedef struct listint_s
 {
-	int n;
+	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
+/** Given FUN prototype */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
+/** TASK Prototype */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-#endif /* _SORT_H_ */
+#endif
